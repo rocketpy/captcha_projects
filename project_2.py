f042aa3fe7cc60e0a7170dff3cc73c8a1ae550dd
rocketpy <dimitr.ushakov@gmail.com>
2021-03-06 21:32:59 +0200

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -31,20 +31,35 @@ cv2.imwrite('res.png', img)
 print(pytesseract.image_to_string('res.png'))
 
 # def get_separ_words():
-try:
-    driver.get(URL)
-    time.sleep(5)
-    search = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea')  # input field                                 
-    search.send_keys(image_word)  # image_word is a string with a word
-    time.sleep(5)
-    search = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]')  # output field
+    try:
+        driver.get(URL)
+        time.sleep(5)
+        search = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea')  # input field                                 
+        search.send_keys(image_word)  # image_word is a string with a word
+        time.sleep(5)
+        search = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]')  # output field
     
-    #  'Perhaps you meant:'
-    #  '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/div[4]/div/div/div/span/html-blob/b/i'
+        #  'Perhaps you meant:'
+        #  '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/div[4]/div/div/div/span/html-blob/b/i'
     
-except Exception as ex:
-    print(ex)
-finally:
-    driver.close()
-    driver.quit()
+        perhaps = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/div[4]/div/div/div/span/html-blob/b/i').text
+    
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
+    
+    
+def get_result(words):
+        result = w2n.word_to_num(words)
+        return result    
+    
+    
+def main():
+    
+    
+if __name__ == '__main__':
+    main()
 
+    
