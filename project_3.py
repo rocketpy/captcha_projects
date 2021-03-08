d7e6f2ad151524990bd48722e50048202e712237
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2021-03-08 23:38:14 +0200

    updated

--- project_3.py
+++ project_3.py
@@ -1,8 +1,8 @@
-# import os
+import os
 import cv2
 import numpy as np
-# import Image
-# from tesseract import image_to_string
+from PIL import Image
+import pytesseract
 
 
 img = cv2.imread("slack_2.jpg")  # or path to file
@@ -17,6 +17,11 @@ third_slice = img[68:102, 30:215]
 # cv2.imshow("Image", img)
 
 im_v = cv2.hconcat([first_slice, second_slice, third_slice])
+
+# result = pytesseract.image_to_string(im_v)
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR'
+# print(pytesseract.image_to_string(Image.open(im_v), lang="rus"))
+# print(result)
 
 cv2.imshow("Image", im_v)
 cv2.waitKey(0)
