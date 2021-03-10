c612a0e0f7f89956ea7a4e9c805c12e315df6901
rocketpy <dimitr.ushakov@gmail.com>
2021-03-10 21:47:32 +0200

    Update Tesseract_Error_Invalid_resolution_0dpi.py

--- Tesseract_Error_Invalid_resolution_0dpi.py
+++ Tesseract_Error_Invalid_resolution_0dpi.py
@@ -1,1 +1,14 @@
+#  How to solve :  Warning: Invalid resolution 0 dpi. Using 70 instead.
 
+# Try using --dpi 300
+
+# Need try to use command
+#  mogrify -set units PixelsPerInch -density 300 image.jpg
+# or
+# mogrify -set density 300 image.jpg
+
+
+# be cause this , can be wrong ...
+# tesseract dpi_issue.png - --dpi 300 --psm 0
+
+#  image_to_osd()
