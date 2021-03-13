a603d598e8b9b7cbd3bc470fae58fb8a4308fc34
rocketpy <dimitr.ushakov@gmail.com>
2021-03-13 23:45:24 +0200

    Update preprocessing_for_Tesseract.py

--- preprocessing_for_Tesseract.py
+++ preprocessing_for_Tesseract.py
@@ -1,1 +1,12 @@
+#  To preprocess image for Tesseract, use any of the following python functions
 
+
+import cv2
+import numpy as np
+
+
+img = cv2.imread('image.jpg')
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
