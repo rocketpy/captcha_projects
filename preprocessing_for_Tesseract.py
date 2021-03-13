264a36a1f4205f14f455ed5dda981f97f199c07d
rocketpy <dimitr.ushakov@gmail.com>
2021-03-13 23:46:29 +0200

    Update preprocessing_for_Tesseract.py

--- preprocessing_for_Tesseract.py
+++ preprocessing_for_Tesseract.py
@@ -10,3 +10,12 @@ img = cv2.imread('image.jpg')
 # get grayscale image
 def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+ 
+# thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
