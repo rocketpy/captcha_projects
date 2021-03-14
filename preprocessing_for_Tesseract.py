c8274288b8a5684b5690abee33fb675937e77b81
rocketpy <dimitr.ushakov@gmail.com>
2021-03-14 22:12:22 +0200

    Update preprocessing_for_Tesseract.py

--- preprocessing_for_Tesseract.py
+++ preprocessing_for_Tesseract.py
@@ -1,6 +1,5 @@
 #  To preprocess image for Tesseract, use any of the following python functions
 
-
 import cv2
 import numpy as np
 
@@ -37,3 +36,18 @@ def opening(image):
 # canny edge detection
 def canny(image):
     return cv2.Canny(image, 100, 200)
+
+# skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+     if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
