faa2582eb7ae5786349bd5b007ed453b9f88844d
rocketpy <dimitr.ushakov@gmail.com>
2021-03-16 21:11:37 +0200

    Update box_around_text.py

--- box_around_text.py
+++ box_around_text.py
@@ -13,3 +13,27 @@ for i in range(n_boxes):
 
 cv2.imshow('img', img)
 cv2.waitKey(0)
+
+"""
+import cv2
+import pytesseract
+
+
+filename = 'image.png'
+
+# read the image and get the dimensions
+img = cv2.imread(filename)
+h, w, _ = img.shape # assumes color image
+
+# run tesseract, returning the bounding boxes
+boxes = pytesseract.image_to_boxes(img) # also include any config options you use
+
+# draw the bounding boxes on the image
+for b in boxes.splitlines():
+    b = b.split(' ')
+    img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+
+# show annotated image and wait for keypress
+cv2.imshow(filename, img)
+cv2.waitKey(0)
+"""
