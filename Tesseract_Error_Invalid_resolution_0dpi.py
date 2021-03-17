560465fa96aed7ecda8d538c91dad579162b972f
rocketpy <dimitr.ushakov@gmail.com>
2021-03-17 23:40:07 +0200

    Update Tesseract_Error_Invalid_resolution_0dpi.py

--- Tesseract_Error_Invalid_resolution_0dpi.py
+++ Tesseract_Error_Invalid_resolution_0dpi.py
@@ -6,6 +6,9 @@
 #  Output to be stored in a text file. 
 # tesseract image.png output.txt
 
+# custom_config = r'-l eng --psm 6'
+# pytesseract.image_to_string(img, config=custom_config)
+
 
 # this not working , lol ...
 #  mogrify -set units PixelsPerInch -density 300 image.jpg
