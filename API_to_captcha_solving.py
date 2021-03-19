047944d79c91db51c50271e9ec7f49ab4b59d013
rocketpy <dimitr.ushakov@gmail.com>
2021-03-19 21:44:44 +0200

    Update API_to_captcha_solving.py

--- API_to_captcha_solving.py
+++ API_to_captcha_solving.py
@@ -1,1 +1,14 @@
+#  Universal API to captcha solving services
+
+# PyPi:  https://pypi.org/project/captcha-solver/
+# pip install captcha-solver
+
+#  Service website is https://2captcha.com?from=3019071
+
+from captcha_solver import CaptchaSolver
+
+
+solver = CaptchaSolver('twocaptcha', api_key='2captcha.com API HERE')
+raw_data = open('captcha.png', 'rb').read()
+print(solver.solve_captcha(raw_data))
 
