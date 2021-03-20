6f0e02669e46f9da485ec3f072ad9eb635bcb957
rocketpy <dimitr.ushakov@gmail.com>
2021-03-20 21:19:39 +0200

    Update Decoding_captchas.py

--- Decoding_captchas.py
+++ Decoding_captchas.py
@@ -7,11 +7,7 @@ im = Image.open("captcha.gif")
 im = im.convert("P")
 print(im.histogram())
 
-
-im = Image.open("captcha.gif")
-im = im.convert("P")
-his = im.histogram()
-
+# And we get the following data: Color and Number of pixels
 values = {}
 
 for i in range(256):
@@ -20,3 +16,21 @@ for i in range(256):
 for j, k in sorted(values.items(), key=itemgetter(1), reverse=True)[:10]:
     print(j, k)
 
+#  change red color letters to black
+"""
+from PIL import Image
+
+
+im = Image.open("captcha.gif")
+im = im.convert("P")
+im2 = Image.new("P", im.size, 255)
+im = im.convert("P")
+temp = {}
+for x in range(im.size[1]):
+    for y in range(im.size[0]):
+        pix = im.getpixel((y, x))
+        temp[pix] = pix
+        if pix == 220or pix == 227:
+            im2.putpixel((y, x), 0)
+im2.save("output.gif")
+"""
