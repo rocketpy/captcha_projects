46ccdc9f34b4d8be89173d3dfe70f862d5746c81
rocketpy <dimitr.ushakov@gmail.com>
2022-02-02 22:33:16 +0200

    Update some_examples.py

--- some_examples.py
+++ some_examples.py
@@ -12,3 +12,18 @@ try:
         EC.presence_of_element_located((By.ID, "some_elem")))
 finally:
     driver.quit()
+    
+    
+# Some solution when a reCaptcha in a login page
+def check_exist_elem(xpath):
+        try:
+            self.driver.find_element_by_xpath(xpath)
+        except NoSuchElementException:
+            return False
+        return True
+
+    login_btn_xpath = 'login_button_xpath'
+    while True:
+        # Here can solve recaptcha during this period and hit the login button.
+        if check_exists_by_xpath(login_btn_xpath) is False:
+            break
