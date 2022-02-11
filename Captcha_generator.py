f0d7a203a0fb59654f30d328ee0525fac8c1d740
rocketpy <dimitr.ushakov@gmail.com>
2022-02-11 23:11:12 +0200

    Update Captcha_generator.py

--- Captcha_generator.py
+++ Captcha_generator.py
@@ -1,1 +1,8 @@
+# Captcha - a captcha library that generates audio and image CAPTCHAs.
+
+# PyPi: https://pypi.org/project/captcha/
+# https://github.com/lepture/captcha/
+
+# pip install captcha
+
 
